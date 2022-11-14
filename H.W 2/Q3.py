3383d1ad85ac7844f47519a95359a691e30dec44
bar-schtalman <barschtalman@gmail.com>
2022-11-14 16:29:28 +0200

    bugs fix

--- H.W 2/Q3.py
+++ H.W 2/Q3.py
@@ -6,7 +6,7 @@ class List(list):
         if isinstance(index,int):
             return super(List, self).__getitem__(index)
         else:
-            ans = list(self)
+            ans = List(self)
             for item in index:
                 ans = ans[item]
             return ans
@@ -15,4 +15,4 @@ class List(list):
 
 
 mylist = List([[[1,2,3,33],[4,5,6,66]],[[7,8,9,99],[10,11,12,122]],[[13,14,15,155],[16,17,18,288]],])
-print(mylist[0,1,3])
+print(mylist[1,1,1])
