1628730cde36a9b11cccf6b68a6c9c4d5d61211d
bar-schtalman <barschtalman@gmail.com>
2022-11-14 16:27:24 +0200

    bugs fix

--- H.W 2/Q1.py
+++ H.W 2/Q1.py
@@ -1,21 +1,22 @@
 import re
-filename = "C:\\Users\\schta\\PycharmProjects\\research_hw1\\H.W 2\\text.txt"
+filename = "C:\\Users\\schta\\PycharmProjects\\research_hw1\\H.W 2\\text4.txt"
 
-lst1 = [1,4,6,3,5,7]
+
 def valid_email(filename):
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    valids = []
+    regex = r'\b([A-Za-z\d]+[._%+-])*([A-Za-z\d])+@([A-Za-z\d-])+\.([A-Z|a-z]{2,})\b'
+    valid = []
     invalids = []
     f = open(filename, "r")
     for line in f:
         for word in line.split():
             if re.fullmatch(regex, word):
-                valids.append(word)
+                valid.append(word)
             elif ('@' or '.') in word:
                 invalids.append(word)
-    print("valid emails addresses:\n",valids)
+    print("valid email addresses:\n",valid)
     print("invalid email addresses:\n",invalids)
     f.close()
+
 
 valid_email(filename)
 
