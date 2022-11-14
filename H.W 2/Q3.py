6f3cfdb841843ed262ef537acccf73c0cdac6d74
bar-schtalman <barschtalman@gmail.com>
2022-11-14 15:15:31 +0200

    q4 added

--- H.W 2/Q3.py
+++ H.W 2/Q3.py
@@ -1,32 +1,18 @@
 class List(list):
-    def __init__(self, lst):
+    def __init__(self,lst):
         super().__init__(lst)
-    def __getitem__(self, index : list):
+
+    def __getitem__(self, index):
         if isinstance(index,int):
-            return super().__getitem__(index)
+            return super(List, self).__getitem__(index)
         else:
-            ans = self
-            for i,item in enumerate(index):
-                #ans.append(super().__getitem__(index[i]))
-                ans = self[item]
+            ans = list(self)
+            for item in index:
+                ans = ans[item]
             return ans
 
 
 
 
-
-
-
-
-
-
-lst2 = List[[[1,2,3,33],[4,5,6,66]],
-    [[7,8,9,99],[10,11,12,122]],
-    [[13,14,15,155],[16,17,18,188]]]
-print(lst2)
-l = [1,2,3]
-ll = [4,5,6]
-for i in ll:
-    l.append(i)
-print(l)
-#print(lst2[0,1,3])
+mylist = List([[[1,2,3,33],[4,5,6,66]],[[7,8,9,99],[10,11,12,122]],[[13,14,15,155],[16,17,18,288]],])
+print(mylist[2,1])
