08e573bdae9099645a918f50d33dd5cd8e2d8b40
bar-schtalman <barschtalman@gmail.com>
2022-11-29 18:43:45 +0200

    ..

--- H.W 3/Q1.py
+++ H.W 3/Q1.py
@@ -1,24 +1,2 @@
-from itertools import *
-
-
-class bounded_subsets:
-
-    def __init__(self,first,end,lst = list,bound = int):
-        self.state = first
-        self.end = end
-        self.bound = bound
-
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.state == self.end:
-            raise StopIteration
-
-        res = self.state
-
-        self.state += self.step
-        return res
 
 
