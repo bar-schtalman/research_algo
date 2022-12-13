d4aab1aa8925a72ca52ee063339424014f8c5bb3
bar-schtalman <barschtalman@gmail.com>
2022-12-14 01:24:46 +0200

    ..

--- H.W 3/Q1.py
+++ H.W 3/Q1.py
@@ -1,37 +1,43 @@
-import itertools
+import sys
+import math
+from itertools import combinations, islice
+from functools import reduce
+from math import comb
+
+"""
+this program solve the "two pile difference" problem from codingame
+https://www.codingame.com/ide/puzzle/the-two-piles-difference
+"""
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+elements = []
+n = int(input())
+for i in input().split():
+    value = int(i)
+    elements.append(value)
+
+lenn = comb(len(elements), len(elements) // 2)
+
+# Generate all possible 3-sized combinations of the numbers
+combinations = combinations(elements, len(elements) // 2)
+half_combos = islice(combinations, 0, lenn)
+
+diff = 9999
+for i in half_combos:
+    temp = list(elements)
+    sumA = 1
+    for s in i:
+        sumA *= s
+    for j in i:
+        temp.remove(j)
+    sumB = sum(temp) ** 2
+    ndiff = abs(sumA - sumB)
+    if ndiff < diff:
+        diff = ndiff
+        if diff == 0:
+            break
+
+print(diff)
 
 
-def bounded_subsets(set:list,num):
-    """
-    this generator calculate all combinatain for subset from size 0 to size num
-    >>> for s in bounded_subsets([1,2,3], 4): print(s, end = '')
-    [][1][2][3][1, 2][1, 3]
-    >>> for s in bounded_subsets([1,2,3,4,5,6],7): print(s, end = '')
-    [][1][2][3][4][5][6][1, 2][1, 3][1, 4][1, 5][1, 6][2, 3][2, 4][2, 5][1, 2, 3][1, 2, 4]
-    >>> for s in bounded_subsets([],2): print(s, end = '')
-    []
-    >>> for s in bounded_subsets(range(1,5),9): print(s, end = '')
-    [][1][2][3][4][1, 2][1, 3][1, 4][2, 3][2, 4][3, 4][1, 2, 3][1, 2, 4][1, 3, 4][2, 3, 4]
-    >>> for s in zip(range(5), bounded_subsets(range(100), 1000000000000)): print(s,end = '')
-    (0, [])(1, [0])(2, [1])(3, [2])(4, [3])
-    >>> for s in bounded_subsets(range(50,150), 103): print(s, end = '')
-    [][50][51][52][53][54][55][56][57][58][59][60][61][62][63][64][65][66][67][68][69][70][71][72][73][74][75][76][77][78][79][80][81][82][83][84][85][86][87][88][89][90][91][92][93][94][95][96][97][98][99][100][101][102][103][50, 51][50, 52][50, 53]
-    """
-    if not isinstance(set,range):
-        set.sort()
-    yield []
-    size = len(set)
-    for i in range(1,size):
-        for combo in (itertools.combinations(set,i)):
-            combo_sum = sum(combo)
-            if combo_sum <= num:
-                yield list(combo)
-            else:
-                break
 
-
-if __name__ == "__main__":
-    import doctest
-
-    (failures, tests) = doctest.testmod(report=True)
-    print("{} failures, {} tests".format(failures, tests))
